61cb13aff86c3c46065b948ced37bad7edc45159
Ben Webb <ben.webb@opendataservices.coop>
2024-03-20 12:07:58 +0000

    convert: Update conversion of OrganisationRegistrationAgency from source
    Improve the diff by:
    * Indenting the output
    
    Maintain changes made to the list in this repo, by:
    * Editing the template in this repo to match changes to the list
    * Maintain the CDATA block during the conversion
    * Output utf-8 instead of amperand encoding

--- convert.py
+++ convert.py
@@ -5,12 +5,16 @@ from lxml import etree
 org_id_xml = etree.parse(join('source', 'org-id-guide.xml'))
 codelist_items = org_id_xml.find('codelist-items')
 
+parser = etree.XMLParser(strip_cdata=False)
 template = etree.parse(
-    join('templates', 'OrganisationRegistrationAgency.xml'))
+    join('templates', 'OrganisationRegistrationAgency.xml'),
+    parser=parser)
 placeholder_codelist_items = template.find('codelist-items')
 
 template.getroot().replace(placeholder_codelist_items, codelist_items)
 
+etree.indent(template, space='    ')
+
 template.write(
     join('xml', 'OrganisationRegistrationAgency.xml'),
-    pretty_print=True)
+    encoding='utf-8')
